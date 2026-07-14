a1dc4b2ec65dbf8fd32473d91cec373bbfa2b784
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-15 01:15:57 +0300

    SearchableK

---
+++ PickerandSearchSwiftUI/SearchableK.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableK.swift
+//  PickerandSearchSwiftUI
+//
+//  Created by Ömer Apaydın on 15.07.2026.
+//
+
+import SwiftUI
+
+struct SearchableK: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchableK()
+}
