5f37e3d58ee226b02c34c38cd50ce6124832b903
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-14 20:48:15 +0300

    Initial Commit

---
+++ PickerandSearchSwiftUI/PickerandSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerandSearchSwiftUIApp.swift
+//  PickerandSearchSwiftUI
+//
+//  Created by Ömer Apaydın on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PickerandSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
